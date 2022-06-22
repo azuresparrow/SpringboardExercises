d1b27c695259384edb267e8ef23d8d0bc77e964f
James Rogers <jrogers2fc@gmail.com>
2022-06-21 19:16:40 -0700

    Destructuring

---
+++ destructuring.js
@@ -0,0 +1,34 @@
+
+8
+1846
+
+//{ yearNeptuneDiscovered: 1846, yearMarsDiscovered: 1659}
+
+//Your name is Alejandro and you like purple
+//Your name is Melissa and you like green
+//Your name is undefined and you like green
+
+Maya
+Marisa
+Chi
+
+"Raindrops on roses"
+
+"whiskers on kittens"
+
+["Bright copper kettles",
+"warm woolen mittens",
+"Brown paper packages tied up with strings"]
+
+[10, 30, 20]
+
+const {a, b} = obj.numbers
+
+let arr = [1,2];
+[arr[1], arr[0]] = [arr[0], arr[1]];
+
+const raceResults = ([first, second, third, ...rest]) => ({first, second, third, rest})
+// ['Tom', 'Margaret', 'Allison', ['David', 'Pierre']] 
+// { first: 'Tom', second: 'Margaret', third: 'Allison', rest: ['David', 'Pierre'] }
+
+raceResults(['Tom', 'Margaret', 'Allison', 'David', 'Pierre']);
